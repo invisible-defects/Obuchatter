413cf07cc389077dbb233e5658d0c01baa255b3d
Alexander Voronkov <myaddictionisguitars@gmail.com>
2018-07-27 23:53:29 +0300

    upd authorize module

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import logging
 
-# This is a minimal configuration to get you started with the Text mode.
-# If you want to connect Errbot to chat services, checkout
-# the options in the more complete config-template.py from here:
-# https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
-
 BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
 BOT_DATA_DIR = r'/home/lifetimealone/code/Obuchatter/data'
--- plugins/authorize/authorize.py
+++ plugins/authorize/authorize.py
@@ -12,7 +12,7 @@ class Authorize(BotPlugin):
 
     @botcmd
     def authorize(self, msg, args):
-        login, password = msg.split()
+        login, password = args.split()
         pas = password.encode('utf-8')
         pas = str(hashlib.md5(pas).hexdigest())
         session = requests.Session()
@@ -20,5 +20,9 @@ class Authorize(BotPlugin):
         auth_url = "https://mrko.mos.ru/dnevnik/services/index.php"
         auth_req = session.get(auth_url, headers=headers, params={"login": login,
             "password": pas}, allow_redirects=False)
-        globals.users[msg.frm] = session
-        return 'Authorized!'
+
+        if auth_req.status_code == 200: # TODO: not a valid check
+            print(session.get("https://mrko.mos.ru/dnevnik/services/dnevnik.php", headers=headers).content)
+            globals.users[msg.frm] = session
+            return 'Авторизован!' # Authorization successful
+        return 'Неправильный логин и\или пароль!' # Authorization not successful
